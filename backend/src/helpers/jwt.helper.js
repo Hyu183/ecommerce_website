3adf0ab38ed9819c8064cee3357786d47adb02c0
Huy Tran-Quang <huytrquang1103@gmail.com>
2022-07-08 17:47:14 +0700

    Update jwt.helper.js

--- backend/src/helpers/jwt.helper.js
+++ backend/src/helpers/jwt.helper.js
@@ -1,22 +1,18 @@
-import jwt from 'jsonwebtoken';
-import { config } from 'dotenv';
-
-//load .env
-config();
+import jwt from "jsonwebtoken";
 
 //payload: id of user
 const generateJWT = (userID, userRole) => {
-    const secretKey = process.env.JWT_SECRET_KEY;
-    const expireTime = process.env.JWT_EXPIRE_TIME;
+  const secretKey = process.env.JWT_SECRET_KEY;
+  const expireTime = process.env.JWT_EXPIRE_TIME;
 
-    return jwt.sign({ id: userID, role: userRole }, secretKey, {
-        expiresIn: expireTime,
-    });
+  return jwt.sign({ id: userID, role: userRole }, secretKey, {
+    expiresIn: expireTime,
+  });
 };
 
 const verifyJWT = (token) => {
-    const secretKey = process.env.JWT_SECRET_KEY;
-    return jwt.verify(token, secretKey);
+  const secretKey = process.env.JWT_SECRET_KEY;
+  return jwt.verify(token, secretKey);
 };
 
 export { generateJWT, verifyJWT };
